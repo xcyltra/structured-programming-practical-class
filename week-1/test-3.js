f904ea8e90a38c36910f5abafb4407218a1f64d0
Laudya Aprilia <luludea95@gmail.com>
2026-03-04 23:06:48 +0800

    test 3

---
+++ week-1/test-3.js
@@ -0,0 +1,8 @@
+const nama = "Faiq";
+const umur = 22;
+const hobi = ["coding", "gaming", "ngecat rumah"];
+
+console.log("Nama: " + nama);
+console.log("Umur: " + umur + " tahun");
+console.log("Hobi: " + hobi);
+console.log({ nama, umur, hobi });   
