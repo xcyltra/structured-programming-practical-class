efd7b814db72d57048a27cd8da428a12b194d3f3
Devina Dian <10241022@student.itk.ac.id>
2026-04-11 11:21:50 +0800

    Create tes.js

---
+++ week-3/tes.js
@@ -0,0 +1,4 @@
+console.log("Expression literal:");
+console.log("5 ->", 5);                    // Expression menghasilkan nilai 5
+console.log("'Hello' ->", 'Hello');        // Expression menghasilkan nilai "Hello" 
+console.log("true ->", true);              // Expression menghasilkan nilai true
