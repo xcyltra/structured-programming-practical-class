73896617f5f60b29030a385c2aec59f345b143c4
Laudya Aprilia <luludea95@gmail.com>
2026-04-14 06:46:46 +0800

    yah

---
+++ week-2/kode.js
@@ -0,0 +1,9 @@
+let nilai = 100;
+let nama siswa = "Budi";
+let $total = 50;
+let for = 10;
+let skor = 90;
+
+// Menampilkan hasil
+/* nilai akhir = $total + skor */
+console.log($total + skor);
---
+++ week-2/kode2.js
@@ -0,0 +1,3 @@
+let Harga = 15000; let Qty = 3; let Diskon = 0.1;
+let Total = Harga * Qty; let Bayar = Total - (Total * Diskon);
+console.log(Bayar);
