8877ffff223215dd779f1f3f8e15aedf9e8e0d0e
Devina Dian <10241022@student.itk.ac.id>
2026-05-18 23:45:47 +0800

    Create 10.arrow_function.js

---
+++ week-10/10.arrow_function.js
@@ -0,0 +1,20 @@
+//arrow function    
+// function sum(a, b) {
+//     return a + b;
+// }
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+// const sum = (a, b) => a + b;
+
+console.log(sum(2, 3));
+
+// const double = n => n * 2;
+
+// console.log(double(5));
+
+// const greet = () => console.log("Hello!");
+
+// greet();
