daff14f6413b0ac3fcc3bd67ea150c0bb133effa
Devina Dian <10241022@student.itk.ac.id>
2026-05-18 23:46:16 +0800

    Create 9.hoisting_js.js

---
+++ week-10/9.hoisting_js.js
@@ -0,0 +1,21 @@
+// sapaSaya();
+
+// function sapaSaya() {
+//     console.log("Halo, saya sedang belajar JavaScript");
+// }
+
+
+// console.log(sapa("arif"));
+
+// function sapa(nama){
+//     return  `Halo, nama saya ${nama}`;  
+// }
+
+
+//function expression
+
+sayHello(); 
+
+const sayHello = function () {
+  console.log("Hello!");
+};
